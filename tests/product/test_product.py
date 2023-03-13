d3dfdcd13a155b5fc3317fa39ebf6107f4a3f586
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-03-12 23:06:33 -0300

    Resolve requisito 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui!
+    product = Product(
+        id=1,
+        nome_do_produto="Product 01",
+        nome_da_empresa="Empresa 01",
+        data_de_fabricacao="2023-03-13",
+        data_de_validade="2024-03-13",
+        numero_de_serie="12345678",
+        instrucoes_de_armazenamento="Cuidado ao armazenar"
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Product 01"
+    assert product.nome_da_empresa == "Empresa 01"
+    assert product.data_de_fabricacao == "2023-03-13"
+    assert product.data_de_validade == "2024-03-13"
+    assert product.numero_de_serie == "12345678"
+    assert product.instrucoes_de_armazenamento == "Cuidado ao armazenar"
