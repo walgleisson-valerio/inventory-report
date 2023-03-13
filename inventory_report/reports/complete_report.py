494ba56579ba100d8a7ced3fa64dd892ff602aef
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-03-13 11:13:47 -0300

    resolve requisito 03

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(list):
+        companies = []
+        for product in list:
+            companies.append(product["nome_da_empresa"])
+
+        companies_stock = Counter(companies)
+        report = ""
+        for company in companies_stock:
+            report += f"- {company}: {companies_stock[company]}\n"
+
+        return (
+            f"{SimpleReport.generate(list)}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{report}"
+        )
