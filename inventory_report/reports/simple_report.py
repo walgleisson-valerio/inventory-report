ca162f492a7794e6d24d81c526903986880aba99
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-03-13 10:36:47 -0300

    resolve requisito 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list):
+        companies = []
+        manufacturing_dates = []
+        expiration_dates = []
+
+        for product in list:
+            companies.append(product["nome_da_empresa"])
+            most_common_company = Counter(companies).most_common()[0][0]
+            manufacturing_dates.append(product["data_de_fabricacao"])
+
+            expiration_date = product["data_de_validade"]
+            date_format = datetime.strptime(expiration_date, "%Y-%m-%d")
+            if datetime.now() < date_format:
+                expiration_dates.append(product["data_de_validade"])
+
+        return (
+            f"Data de fabricação mais antiga: {min(manufacturing_dates)}\n"
+            f"Data de validade mais próxima: {min(expiration_dates)}\n"
+            f"Empresa com mais produtos: {most_common_company}"
+        )
